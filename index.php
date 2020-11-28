b77fcc537d987e8ca840193a2c03b4f4f3fc150d
Leo <leo.manucci@gmail.com>
2020-11-28 15:57:40 -0300

    se cambia tabla por grilla

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ml-auto">
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Portfolio</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#team_members">Team Members</a></li>
                         <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">About</a></li>
                         <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact</a></li>
                     </ul>
@@ -61,6 +61,25 @@
                 die("Could not connect to the database mysql :" . $e->getMessage());
             }
         ?>
+		<section class="page-section team_members" id="team_members">
+            <div class="container">
+                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Team Members</h2>
+                <div class="divider-custom">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><svg class="svg-inline--fa fa-star fa-w-18" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg><!-- <i class="fas fa-star"></i> Font Awesome fontawesome.com --></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <div class="row justify-content-center">
+                <?php foreach($team_members as $member): ?>
+                    <div class="col-md-6 col-lg-4 mb-5">
+					<h4 class="text-uppercase mb-2"><?=$member['nombre'] ?></h4>
+                    </div>
+                <?php endforeach?>
+                </div>
+            </div>
+        </section>
+
+
         <table>
             <thead>
                 <tr>
@@ -68,11 +87,6 @@
                 </tr>
             </thead>
             <tbody>
-                <?php foreach($team_members as $member): ?>
-                    <tr>
-                        <td><?=$member['nombre'] ?></td>
-                    </tr>
-                <?php endforeach?>
             </tbody>
         </table>
         <!-- Bootstrap core JS-->
