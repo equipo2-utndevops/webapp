37eb7c8724882688171d1370ce9c09b5e207db69
Leo <leo.manucci@gmail.com>
2020-12-09 18:07:56 -0300

    se instala laravel 6

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="css/styles.css" rel="stylesheet" />
+        <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
     </head>
     <body id="page-top">
         <!-- Navigation-->
@@ -38,7 +38,7 @@
         <header class="masthead bg-primary text-white text-center">
             <div class="container d-flex align-items-center flex-column">
                 <!-- Masthead Avatar Image-->
-                <img src="assets/img/teamwork.png" alt="" />
+                <img src="{{ asset('assets/img/teamwork.png') }}" alt="" />
                 <!-- Icon Divider-->
                 <div class="divider-custom divider-light">
                     <div class="divider-custom-line"></div>
@@ -46,55 +46,15 @@
                     <div class="divider-custom-line"></div>
                 </div>
                 <!-- Masthead Subheading-->
-                <p class="masthead-subheading font-weight-light mb-0">Practica 02</p>
+                <p class="masthead-subheading font-weight-light mb-0">Practica 04</p>
             </div>
         </header>
-        <?php
-            try{
-                $pdo = new PDO("mysql:host=db;dbname=db_devops","root","root");
-                $stmt = $pdo->prepare("SELECT nombre from team_members");
-                $stmt->execute();
-                $team_members= $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-            }
-            catch(PDOException $e){
-                die("Could not connect to the database mysql :" . $e->getMessage());
-            }
-        ?>
-		<section class="page-section team_members" id="team_members">
-            <div class="container">
-                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Team Members</h2>
-                <div class="divider-custom">
-                    <div class="divider-custom-line"></div>
-                    <div class="divider-custom-icon"><svg class="svg-inline--fa fa-star fa-w-18" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg><!-- <i class="fas fa-star"></i> Font Awesome fontawesome.com --></div>
-                    <div class="divider-custom-line"></div>
-                </div>
-                <div class="row justify-content-center">
-                <?php foreach($team_members as $member): ?>
-                    <div class="col-md-6 col-lg-4 mb-5">
-					<h4 class="text-uppercase mb-2"><?=$member['nombre'] ?></h4>
-                    </div>
-                <?php endforeach?>
-                </div>
-            </div>
-        </section>
-
-
-        <table>
-            <thead>
-                <tr>
-                    <th>Team Members</th>
-                </tr>
-            </thead>
-            <tbody>
-            </tbody>
-        </table>
         <!-- Bootstrap core JS-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Third party plugin JS-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
         <!-- Core theme JS-->
-        <script src="js/scripts.js"></script>
+        <script src="{{ asset('js/scripts.js') }}"></script>
     </body>
 </html>
